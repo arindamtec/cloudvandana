a2bb5d417a777028e99a5a8835d9020062eada8c
arindamtec <arindamspectre@gmail.com>
2023-10-16 17:22:05 +0530

    java sets are done.

---
+++ java codes/p3.java
@@ -0,0 +1,29 @@
+public class p3 {
+    public static boolean isPangram(String input) {
+        boolean[] alphabet = new boolean[26];
+
+        input = input.toLowerCase();
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+            if (ch >= 'a' && ch <= 'z') {
+                alphabet[ch - 'a'] = true;
+            }
+        }
+        for (boolean letterPresent : alphabet) {
+            if (!letterPresent) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        String input = "The quick brown fox jumps over the lazy dog";
+        boolean isPangram = isPangram(input);
+
+        if (isPangram) {
+            System.out.println("The input is a pangram.");
+        } else {
+            System.out.println("The input is not a pangram.");
+        }
+    }
+}
